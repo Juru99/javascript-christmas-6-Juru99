49d2d47f5534ae5c0d70f43a18796dd20fcd798d
stegano9744 <stegano9744@gmail.com>
2023-11-14 02:35:59 +0900

    ✨ feat(OutputView.js): 출력 기능 구현
    출력 목록:
    - 이벤트 플래너
    - 미리 보기
    - 주문 메뉴
    - 할인 전 총 주문 금액
    - 증정 메뉴
    - 혜택 내역
    - 총혜택 금액
    - 할인 후 예상 결제 금액
    - 이벤트 배지
    - 이벤트 미적용

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,9 +1,76 @@
+import { Console } from '@woowacourse/mission-utils';
+import Utils from '../utils/Utils';
+
 const OutputView = {
-  printMenu() {
-    Console.print('<주문 메뉴>');
-    // ...
+  printEventPlanner() {
+    Console.print('안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.');
   },
-  // ...
+
+  printPreview(visitDate) {
+    Console.print(
+      `12월 ${visitDate}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`,
+    );
+  },
+
+  printMenu(menus) {
+    Console.print('\n<주문 메뉴>');
+
+    menus.forEach((menu) => {
+      Console.print(`${menu[0]} ${menu[1]}개`);
+    });
+  },
+
+  printTotalAmount(totalAmount) {
+    Console.print('\n<할인 전 총주문 금액>');
+    Console.print(`${Utils.separateThousand(totalAmount)}원`);
+  },
+
+  printGiveawayMenu(totalAmount) {
+    let giveawayMenu = '없음';
+    if (totalAmount >= 120_000) giveawayMenu = '샴페인 1개';
+
+    Console.print('\n<증정 메뉴>');
+    Console.print(giveawayMenu);
+  },
+
+  printBenefitDetails(totalBenefit) {
+    Console.print('\n<혜택 내역>');
+
+    for (const [benefit, amount] of Object.entries(totalBenefit)) {
+      Console.print(`${benefit}: -${Utils.separateThousand(amount)}원`);
+    }
+  },
+
+  printTotalBenefit(totalBenefit) {
+    Console.print('\n<총혜택 금액>');
+    Console.print(`-${Utils.separateThousand(totalBenefit)}원`);
+  },
+
+  printExpectedPaymentAmount(expectedPaymentAmount) {
+    Console.print('\n<할인 후 예상 결제 금액>');
+    Console.print(`${Utils.separateThousand(expectedPaymentAmount)}원`);
+  },
+
+  printEventBadge(totalBenefit) {
+    let eventBadge = '없음';
+
+    if (totalBenefit >= 20000) eventBadge = '산타';
+    else if (totalBenefit >= 10000) eventBadge = '트리';
+    else if (totalBenefit >= 5000) eventBadge = '별';
+
+    Console.print('\n<12월 이벤트 배지>');
+    Console.print(eventBadge);
+  },
+
+  printNoEvent(totalAmount) {
+    Console.print('\n<증정 메뉴>\n없음');
+    Console.print('\n<혜택 내역>\n없음');
+    Console.print('\n<총혜택 금액>\n0원');
+    Console.print(
+      `\n<할인 후 예상 결제 금액>\n${Utils.separateThousand(totalAmount)}원`,
+    );
+    Console.print('\n<12월 이벤트 배지>\n없음');
+  },
 };
 
 export default OutputView;
