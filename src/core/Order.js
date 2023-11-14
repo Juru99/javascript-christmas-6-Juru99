4116c6746c63d4ff9eaf9c4a17b968bf75bfcb38
stegano9744 <stegano9744@gmail.com>
2023-11-14 18:30:30 +0900

    ✨ feat(Order.js): 총주문 금액 계산 구현
    작업 목록:
    - 주문 메뉴 출력
    - 총주문 금액 계산
    - 총주문 금액 출력
    - 총주문 금액 반환

--- src/core/Order.js
+++ src/core/Order.js
@@ -1,8 +1,42 @@
-class Order {
-  calculateTotalAmount(menu) {
-    let totalAmount = 0;
+import { MENU } from '../constants/constants';
+import Utils from '../utils/Utils';
+import OutputView from '../views/OutputView';
 
-    return totalAmount;
+class Order {
+  #menus;
+  #totalAmount;
+
+  constructor() {
+    this.#totalAmount = 0;
+  }
+
+  calculateTotalAmount(menu) {
+    this.#orderMenu(menu);
+    this.#calculateMenu();
+    OutputView.printTotalAmount(this.#totalAmount);
+
+    return this.#totalAmount;
+  }
+
+  #orderMenu(menu) {
+    this.#menus = Utils.separateMenu(menu);
+    OutputView.printMenu(this.#menus);
+  }
+
+  #calculateMenu() {
+    const menuNames = this.#menus.map((manuItem) => manuItem[0]);
+    const menuCount = Array.from(
+      this.#menus.map((manuItem) => manuItem[1]),
+      Number,
+    );
+    const menuPrice = menuNames.map((menuName) => {
+      const menuItem = MENU.find((item) => item.name === menuName);
+      return menuItem ? menuItem.price : null;
+    });
+
+    menuPrice.forEach((item, i) => {
+      this.#totalAmount += item * menuCount[i];
+    });
   }
 }
 
