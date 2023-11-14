150763a2ddf48e3703c0259147161e24bd4f0789
stegano9744 <stegano9744@gmail.com>
2023-11-14 13:42:14 +0900

    ✨ feat(Utils.js): 유틸리티 함수 구현
    함수 목록:
    - 날짜에 대응되는 요일 계산
    - 입력받은 메뉴에서 주문 메뉴와 개수 분리
    - 금액을 천 단위로 ',(쉼표)'를 붙임

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,3 +1,23 @@
-class Utils {}
+class Utils {
+  static calculateVisitDay(visitDate) {
+    const date = new Date(`2023-12-${visitDate}`);
+
+    return date.getDay();
+  }
+
+  static separateMenu(menu) {
+    const orderMenu = [];
+
+    menu.split(',').forEach((v) => {
+      orderMenu.push(v.split('-'));
+    });
+
+    return orderMenu;
+  }
+
+  static separateThousand(amount) {
+    return String(amount).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  }
+}
 
 export default Utils;
