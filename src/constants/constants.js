fc38c167f2506f9b471ec354badde2de7c3404aa
stegano9744 <stegano9744@gmail.com>
2023-11-14 22:48:30 +0900

    ✨ feat(Discount.js): 할인 계산 구현
    작업 목록:
    - 크리스마스 디데이 할인 계산
    - 평일 할인 계산
    - 주말 할인 계산
    - 특별 할인
    - 증정 이벤트
    - MENU 상수 데이터 금액 천 단위 _ 추가
    - DATE 상수 데이터 추가
    - PRICE 상수 데이터 추가

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,62 +1,78 @@
 export const MENU = [
   {
     name: '양송이수프',
-    price: 6000,
+    price: 6_000,
     category: '애피타이저',
   },
   {
     name: '타파스',
-    price: 5500,
+    price: 5_500,
     category: '애피타이저',
   },
   {
     name: '시저샐러드',
-    price: 8000,
+    price: 8_000,
     category: '애피타이저',
   },
   {
     name: '티본스테이크',
-    price: 55000,
+    price: 55_000,
     category: '메인',
   },
   {
     name: '바비큐립',
-    price: 54000,
+    price: 54_000,
     category: '메인',
   },
   {
     name: '해산물파스타',
-    price: 35000,
+    price: 35_000,
     category: '메인',
   },
   {
     name: '크리스마스파스타',
-    price: 25000,
+    price: 25_000,
     category: '메인',
   },
   {
     name: '초코케이크',
-    price: 15000,
+    price: 15_000,
     category: '디저트',
   },
   {
     name: '아이스크림',
-    price: 5000,
+    price: 5_000,
     category: '디저트',
   },
   {
     name: '제로콜라',
-    price: 3000,
+    price: 3_000,
     category: '음료',
   },
   {
     name: '레드와인',
-    price: 60000,
+    price: 60_000,
     category: '음료',
   },
   {
     name: '샴페인',
-    price: 25000,
+    price: 25_000,
     category: '음료',
   },
 ];
+
+export const DATE = {
+  weekday: [0, 1, 2, 3, 4],
+  weekend: [5, 6],
+  special: [3, 10, 17, 24, 25, 31],
+  eventStart: 1,
+  eventEnd: 25,
+};
+
+export const PRICE = {
+  christmasDDayStartDiscount: 1_000,
+  week: 2_023,
+  special: 1_000,
+  payment: 120_000,
+  freeGift: 25_000,
+};
--- src/core/Discount.js
+++ src/core/Discount.js
@@ -1,8 +1,69 @@
+import { DATE, MENU, PRICE } from '../constants/constants';
+import Utils from '../utils/Utils';
+import OutputView from '../views/OutputView';
+
 class Discount {
-  #totalBenefit;
+  #totalBenefit = [];
 
   calculateDiscount(totalAmount, visitDate, menu) {
+    OutputView.printGiveawayMenu(totalAmount);
+    const visitDay = Utils.calculateVisitDay(visitDate);
+
+    if (visitDate >= DATE.eventStart && visitDate <= DATE.eventEnd) {
+      this.#christmasDDayDiscount(visitDate);
+    }
+    DATE.weekday.includes(visitDay) && this.#weekdayDiscount(menu);
+    DATE.weekend.includes(visitDay) && this.#weekendDiscount(menu);
+    DATE.special.includes(visitDate) && this.#specialDiscount();
+    totalAmount >= PRICE.payment && this.#giveawayEventDiscount();
+
+    OutputView.printBenefitDetails(this.#totalBenefit);
+
     return this.#totalBenefit;
+  }
+
+  #christmasDDayDiscount(visitDate) {
+    let discount = PRICE.christmasDDayStartDiscount;
+    discount += (Number(visitDate) - 1) * 100;
+
+    this.#totalBenefit.push(['크리스마스 디데이 할인', discount]);
+  }
+
+  #weekdayDiscount(menu) {
+    let discount = 0;
+    const dessert = [];
+    MENU.map((item) => item.category === '디저트' && dessert.push(item.name));
+
+    Utils.separateMenu(menu).forEach((item) => {
+      if (dessert.includes(item[0])) {
+        discount += PRICE.week * Number(item[1]);
+      }
+    });
+
+    discount !== 0 && this.#totalBenefit.push(['평일 할인', discount]);
+  }
+
+  #weekendDiscount(menu) {
+    let discount = 0;
+    const main = [];
+
+    MENU.map((item) => item.category === '메인' && main.push(item.name));
+
+    Utils.separateMenu(menu).forEach((item) => {
+      if (main.includes(item[0])) {
+        discount += PRICE.week * Number(item[1]);
+      }
+    });
+
+    discount !== 0 && this.#totalBenefit.push(['주말 할인', discount]);
+  }
+
+  #specialDiscount() {
+    this.#totalBenefit.push(['특별 할인', PRICE.special]);
+  }
+
+  #giveawayEventDiscount() {
+    this.#totalBenefit.push(['증정 이벤트', PRICE.freeGift]);
   }
 }
 
